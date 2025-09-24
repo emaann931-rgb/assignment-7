b04874483ce3ade0b7b0be7df3d3ce0458da73f3
Noor13456 <noorgrps@gmail.com>
2025-09-24 15:23:36 +0500

    1

---
+++ activity 1.py
@@ -0,0 +1,19 @@
+#convert the following into boolean
+bool("True")
+bool("False")
+bool("0")
+bool("1")
+bool(0)
+bool(1)
+
+#Convert a string to an integer
+integervalue=int(float("4.5869"))
+print(integervalue)
+
+#Convert to float
+floattrue=float(True)
+print(floattrue)
+floatfalse=float(False)
+print(floatfalse)
+floatPython=float(Python)
+print(floatPython)
